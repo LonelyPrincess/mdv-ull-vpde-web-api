d8b41618bf3611c91933574c0795827d37fbc1a6
Sara <sara.her.su@gmail.com>
2024-02-03 23:27:59 +0000

    feat: implement storage to preserve scores

--- src/index.js
+++ src/index.js
@@ -1,15 +1,85 @@
-console.log('Hello! :3');
-
 const gameArea = document.getElementById('game-area');
 const character = document.getElementById('character');
 
 const [currentScoreInput] = document.getElementsByName('current-score');
 const [accumulatedScoreInput] = document.getElementsByName('accumulated-score');
 
+const storage = (() => {
+  const getFromLocalStorage = key => {
+    if (!!localStorage) {
+      return localStorage.getItem(key);
+    } else {
+      return null;
+    }
+  };
+
+  const getFromSessionStorage = key => {
+    if (!!sessionStorage) {
+      return sessionStorage.getItem(key);
+    } else {
+      return null;
+    }
+  };
+
+  const setInLocalStorage = (key, value) => {
+    if (!!localStorage) {
+      return localStorage.setItem(key, value);
+    }
+  };
+
+  const setInSessionStorage = (key, value) => {
+    if (!!sessionStorage) {
+      return sessionStorage.setItem(key, value);
+    }
+  };
+
+  return {
+    getFromLocalStorage,
+    getFromSessionStorage,
+    setInLocalStorage,
+    setInSessionStorage,
+  };
+})();
+
+const scoreManager = (() => {
+  let currentScore = 0;
+  let accumulatedScore = 0;
+
+  const updateScoreUI = () => {
+    currentScoreInput.value = currentScore;
+    accumulatedScoreInput.value = accumulatedScore;
+  };
+
+  const initializeScore = () => {
+    currentScore = storage.getFromSessionStorage('score') || 0;
+    accumulatedScore = storage.getFromLocalStorage('score') || 0;
+
+    updateScoreUI();
+  };
+
+  // Increment score and update storage with the new values
+  const incrementScore = () => {
+    currentScore++;
+    storage.setInSessionStorage('score', currentScore);
+
+    accumulatedScore++;
+    storage.setInLocalStorage('score', accumulatedScore);
+
+    updateScoreUI();
+  };
+
+  return {
+    initializeScore,
+    incrementScore,
+  };
+})();
+
+scoreManager.initializeScore();
+
 // Listen to animation end event to know when jump has ended
 character.addEventListener("animationend", () => {
   console.log({ currentValue: currentScoreInput.value, nextValue: +currentScoreInput.value + 1 });
-  currentScoreInput.value = +currentScoreInput.value + 1;
+  scoreManager.incrementScore();
   character.classList.remove('jumping');
 });
 
