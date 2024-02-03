26344ab3785c37fcceabd906a055eebfca8698df
Sara <sara.her.su@gmail.com>
2024-02-03 20:26:18 +0000

    feat: update current score count

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ console.log('Hello! :3');
 const gameArea = document.getElementById('game-area');
 const character = document.getElementById('character');
 
+const [currentScoreInput] = document.getElementsByName('current-score');
+const [accumulatedScoreInput] = document.getElementsByName('accumulated-score');
+
 // Listen to pressed keys
 document.addEventListener('keydown', (e) => {
   switch (e.key) {
@@ -19,8 +22,11 @@ document.addEventListener('keydown', (e) => {
       console.log('Shall trigger jump...');
       if (!character.classList.contains('jumping')) {
         character.classList.add('jumping');
+        // TODO: bug! we're adding a new listener on every jump, but we only want one
         character.addEventListener("animationend", () => {
           console.log("Animation ended");
+          console.log({ currentValue: currentScoreInput.value, nextValue: +currentScoreInput.value + 1 });
+          currentScoreInput.value = +currentScoreInput.value + 1;
           character.classList.remove('jumping');
         });
       } else {
